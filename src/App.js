eed773b9703823305e8cf8a70d8764c297473cc2
unknown <den.timajo@gmail.com>
2020-06-29 10:15:35 +0800

    Added list of todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const TodoForm = ({handleSubmit, setInputText}) => {
+const TodoForm = ({handleSubmit, inputText, setInputText}) => {
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" onChange={event => setInputText(event.target.value)} />
+      <input type="text" value={inputText} onChange={event => setInputText(event.target.value)} />
       <button type="submit">Add</button>
     </form>)
+}
+
+const TodoList = ({todoList}) => {
+  return (
+    <div>
+      {todoList.map(item => <div>{item}</div>)}
+    </div>
+  )
 }
 
 const TodoListApp = props => {
@@ -16,13 +24,17 @@ const TodoListApp = props => {
 
   const handleSubmit = (event) => {
     setTodoList([...todoList, inputText])
-    console.log(todoList)
+    setInputText("")
     event.preventDefault()
   }
 
   return (
   <div>
-    <TodoForm handleSubmit={handleSubmit} setInputText={setInputText}/>
+    <TodoForm 
+      handleSubmit={handleSubmit} 
+      inputText={inputText} 
+      setInputText={setInputText}/>
+    <TodoList todoList={todoList} />
   </div>)
 
 }
